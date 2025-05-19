8c94030cc2501894d488cee3b9610e32adbf4cd9
angel <angelskyAndHell@gmail.com>
2025-05-18 21:18:35 -0400

    frontend

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from flask_pymongo import PyMongo
 from flask_cors import CORS
-
 mongo = PyMongo()
 
 def create_app():
@@ -11,14 +10,14 @@ def create_app():
     mongo.init_app(app)
     CORS(app)
 
-    # Importar todos los blueprints
     from .routes.usuario_routes import usuario_bp
     from .routes.ingredientes import ingredientes_bp
     from .routes.recetas import recetas_bp
+    from .routes.views import views_bp
 
-    # Registrar todos los blueprints
     app.register_blueprint(usuario_bp)
     app.register_blueprint(ingredientes_bp)
     app.register_blueprint(recetas_bp)
+    app.register_blueprint(views_bp)
 
     return app
---
+++ app/routes/views.py
@@ -0,0 +1,53 @@
+from flask import Blueprint, render_template, request, redirect, url_for
+from bson import ObjectId
+from app import mongo
+
+views_bp = Blueprint('views', __name__)
+
+@views_bp.route('/')
+def home():
+    recetas = list(mongo.db.recetas.find())
+    return render_template('index.html', recetas=recetas)
+
+@views_bp.route('/receta')
+def lista_recetas():
+    recetas = list(mongo.db.recetas.find())
+    return render_template('recetas.html', recetas=recetas)
+
+@views_bp.route('/usuario')
+def lista_usuarios():
+    usuarios = list(mongo.db.usuarios.find())
+    return render_template('usuario.html', usuarios=usuarios)
+
+@views_bp.route('/receta/<id>', methods=['GET', 'POST'])
+def ver_receta(id):
+    receta = mongo.db.recetas.find_one({"_id": ObjectId(id)})
+    if not receta:
+        return "Receta no encontrada", 404
+
+    # Obtener nombres de ingredientes
+    ingredientes = []
+    ingredientes = []
+    for ing_id in receta.get("ingredientes_ids", []):
+        ing = mongo.db.ingredientes.find_one({"_id": ObjectId(ing_id)})
+        if ing:
+            ingredientes.append({"nombre": ing.get("nombre", "Desconocido")})
+            
+    # Agregar comentario
+    if request.method == 'POST':
+        nombre_usuario = request.form.get('nombre')
+        comentario_texto = request.form.get('comentario')
+
+        nuevo_comentario = {
+            "usuario": nombre_usuario,
+            "comentario": comentario_texto
+        }
+
+        mongo.db.recetas.update_one(
+            {"_id": ObjectId(id)},
+            {"$push": {"comentarios": nuevo_comentario}}
+        )
+
+        return redirect(url_for('views.ver_receta', id=id))
+
+    return render_template('detalle_receta.html', receta=receta, ingredientes=ingredientes)
